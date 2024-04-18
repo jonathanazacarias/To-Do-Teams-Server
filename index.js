87e8c8978d03403cd27ee5a3f1d3d4fdf1d805a4
Jonathan Zacarias <jonathanazacarias@gmail.com>
2024-04-18 16:55:21 -0700

    Use cors middleware and switch from body-parser to built in express.json()

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 import express from "express";
 import pg from "pg";
-import bodyParser from "body-parser";
 import bcrypt from "bcrypt";
 import passport from "passport";
 import { Strategy } from "passport-local";
 import GoogleStrategy from "passport-google-oauth2";
 import session from "express-session";
 import env from "dotenv";
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 const saltRounds = 10;
 env.config();
+
+app.use(cors());
+
+app.use(express.json());
 
 app.use(
     session({
@@ -33,10 +37,21 @@ const db = new pg.Client({
 });
 db.connect();
 
+app.get('/', (req, res) => {
+    res.set("Access-Control-Allow-Origin", "*");
+    res.send('Welcome to to-do server');
+})
+
 app.post('/login', (req, res) => {
     console.log(req.body);
     res.set("Access-Control-Allow-Origin", "*");
-    res.send('Got to login!')
+    res.send('Got to login!');
+});
+
+app.post("/register", (req, res) => {
+  console.log(req.body);
+  res.set("Access-Control-Allow-Origin", "*");
+  res.send("Got to register!");
 });
 
 app.listen(port, () => {
