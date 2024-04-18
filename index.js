34023c58fc7423b06c210435fedb1a22b7057b77
Jonathan Zacarias <jonathanazacarias@gmail.com>
2024-04-18 15:28:56 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import pg from "pg";
+import bodyParser from "body-parser";
+import bcrypt from "bcrypt";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
+import session from "express-session";
+import env from "dotenv";
+
+const app = express();
+const port = 3000;
+const saltRounds = 10;
+env.config();
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const db = new pg.Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+db.connect();
+
+app.post('/login', (req, res) => {
+    console.log(req.body);
+    res.set("Access-Control-Allow-Origin", "*");
+    res.send('Got to login!')
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}.`);
+})
