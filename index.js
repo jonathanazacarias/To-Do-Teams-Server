05bb4034d93ebb36835ad5f6bbcd08d373ba65ad
Jonathan Zacarias <jonathanazacarias@gmail.com>
2024-05-07 16:12:55 -0700

    Add delete list route

--- index.js
+++ index.js
@@ -8,63 +8,6 @@ import session from "express-session";
 import env from "dotenv";
 import cors from "cors";
 
-const listsList = [
-  {
-    id: "1",
-    title: "List 1",
-    description: "A list of thing to do",
-    items: [
-      {
-        id: "1",
-        title: "Get milk",
-        description: "Go to the store to get milk",
-      },
-      {
-        id: "2",
-        title: "Get dog food",
-        description: "Go to store for dog food",
-      },
-      {
-        id: "3",
-        title: "Walk dog",
-        description: "Take the dog outside for a walk",
-      },
-    ],
-    owner: { userId: "123456", userName: "jon", avatar: "" },
-    contributers: [],
-    created: "2024-04-14T14:30:15.449Z",
-    modified: "2024-04-14T14:38:15.449Z",
-    modifiedBy: { userId: "123456", userName: "jon", avatar: "" },
-  },
-  {
-    id: "2",
-    title: "List 2",
-    description: "A list of thing to do",
-    items: [
-      {
-        id: "1",
-        title: "Get cheese",
-        description: "Go to the store to get cheese",
-      },
-      {
-        id: "2",
-        title: "Get cat food",
-        description: "Go to store for cat food",
-      },
-      {
-        id: "3",
-        title: "Walk cat",
-        description: "Take the cat outside for a walk",
-      },
-    ],
-    owner: { userId: "123456", userName: "jon", avatar: "" },
-    contributers: [{ userId: "654321", userName: "car", avatar: "" }],
-    created: "2024-04-14T14:30:15.449Z",
-    modified: "2024-04-14T14:38:15.449Z",
-    modifiedBy: { userId: "123456", userName: "jon", avatar: "" },
-  },
-];
-
 const app = express();
 const port = 3000;
 const saltRounds = 10;
@@ -215,7 +158,9 @@ app.post("/lists", (req, res) => {
   if (req.isAuthenticated) {
     try {
       const newList = db.query(
-        "INSERT INTO lists (id, list_owner_id, title, description, created_date, modified_date, modified_by) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+        `INSERT INTO lists 
+        (id, list_owner_id, title, description, created_date, modified_date, modified_by) 
+        VALUES ($1, $2, $3, $4, $5, $6, $7)`,
         [
           req.body.id,
           req.body.owner.id,
@@ -282,14 +227,24 @@ app.put("/lists", async (req, res) => {
         // there should never be an item in the old list that does not exist in the new list,
         // therefore we should always be able to find the old list item in the new list, if we
         // dont then there way and error and we send error status
-        if(!itemFromNew) {
+        if (!itemFromNew) {
           res.sendStatus(500);
         }
 
-        if(oldListItems[i].title !== itemFromNew.title || oldListItems[i].description !== itemFromNew.description) {
-          await db.query("UPDATE list_items SET id=$1, list_id=$2, title=$3, description=$4 WHERE id=$5",
-            [itemFromNew.id, id, itemFromNew.title, itemFromNew.description, itemFromNew.id]
-          )
+        if (
+          oldListItems[i].title !== itemFromNew.title ||
+          oldListItems[i].description !== itemFromNew.description
+        ) {
+          await db.query(
+            "UPDATE list_items SET id=$1, list_id=$2, title=$3, description=$4 WHERE id=$5",
+            [
+              itemFromNew.id,
+              id,
+              itemFromNew.title,
+              itemFromNew.description,
+              itemFromNew.id,
+            ]
+          );
         }
       }
 
@@ -299,6 +254,22 @@ app.put("/lists", async (req, res) => {
     }
   } else {
     res.sendStatus(403);
+  }
+});
+
+app.delete("/lists/:listId", async (req, res) => {
+  if (req.isAuthenticated) {
+    try {
+      const listId = req.params.listId;
+      await db.query(
+        `DELETE FROM lists WHERE id= $1`,
+        [listId]
+      );
+      res.sendStatus(200);
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(500);
+    }
   }
 });
 
